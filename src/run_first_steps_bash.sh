4fcc9280bf0ba9477a0a8f9d647939a314ba2f15
svmueller <38595245+svmueller@users.noreply.github.com>
2020-12-16 16:56:27 +0100

    Update for v2020-12 / Lunik

--- src/run_first_steps_bash.sh
+++ src/run_first_steps_bash.sh
@@ -8,7 +8,7 @@ wget --server-response --quiet --body-file=authentication_request.json --output-
 ACCESS_TOKEN_VALUE=`cat token.json | jq --raw-output .access_token`
 
 # Send your request to the server. You can change the request in the pass_prediction_request.json file.
-wget --content-on-error --output-document=request_response.json --body-file=pass_prediction_request.json --header="Content-Type: application/json" --header="Accept: application/json" --header="Authentication: Bearer ${ACCESS_TOKEN_VALUE}" --header="Authorization: Bearer ${ACCESS_TOKEN_VALUE}"  --method=POST https://platform.okapiorbits.com/api/predict-passes/sgp4/requests
+wget --content-on-error --output-document=request_response.json --body-file=pass_prediction_request.json --header="Content-Type: application/json" --header="Accept: application/json" --header="Authentication: Bearer ${ACCESS_TOKEN_VALUE}" --header="Authorization: Bearer ${ACCESS_TOKEN_VALUE}"  --method=POST https://api.okapiorbits.com/predict-passes/sgp4/requests
 
 # wait a moment to let the server process the request
 sleep 3
@@ -17,7 +17,7 @@ sleep 3
 request_id=`cat request_response.json | jq '.request_id' | sed -e 's/^"//' -e 's/"$//'`
 
 # Contact the server and retrieve the pass prediction result with the extracted request id
-wget --content-on-error --output-document=pass_prediction_result.json --header="access_token: ${ACCESS_TOKEN_VALUE}" --header="Authorization: Bearer ${ACCESS_TOKEN_VALUE}" https://platform.okapiorbits.com/api/predict-passes/sgp4/results/${request_id}/summary
+wget --content-on-error --output-document=pass_prediction_result.json --header="access_token: ${ACCESS_TOKEN_VALUE}" --header="Authorization: Bearer ${ACCESS_TOKEN_VALUE}" https://api.okapiorbits.com/predict-passes/sgp4/results/${request_id}/summary
 
 # Wait, so that you can read the server response
 sleep 3
